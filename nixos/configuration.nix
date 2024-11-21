77dbe5b8a5914b677277b3b52c27b3626d90534a
tu-leminh <51118002+tu-leminh@users.noreply.github.com>
2024-11-21 14:03:39 +0700

    update

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,24 +1,50 @@
 { config, lib, pkgs, ... }:
 let
-        proxy="http://tulm9:EprSu3WQq5%402024@10.205.0.150:8080/";
+proxy="http://tulm9:EprSu3WQq5%402024@10.205.0.150:8080/";
 in
 {
-  imports = [
-    # include NixOS-WSL modules
-    <nixos-wsl/modules>
-  ];
-  networking.proxy.httpProxy = proxy;
-  networking.proxy.httpsProxy = proxy;
-  environment.variables =
-  {
-    HTTP_PROXY = proxy;
-    http_proxy = proxy;
-    https_proxy = proxy;
-    HTTPS_proxy = proxy;
-  };
-  wsl.enable = true;
-  wsl.defaultUser = "nixos";
-  system.stateVersion = "24.05";
+	imports = [
+		<nixos-wsl/modules>
+	];
+	networking.proxy.httpProxy = proxy;
+	networking.proxy.httpsProxy = proxy;
+	environment.variables =
+	{
+		HTTP_PROXY = proxy;
+		http_proxy = proxy;
+		https_proxy = proxy;
+		HTTPS_proxy = proxy;
+	};
+	wsl.enable = true;
+	wsl.defaultUser = "mt";
+	users.users.mt = 
+	{
+		isNormalUser = true;
+		description = "mt";
+		extraGroups = [ "networkmanager" "wheel" ];
+		packages = with pkgs; 
+		[
+		];
+		shell = pkgs.fish;
+	};
+	environment.systemPackages = with pkgs; 
+	[
+		neovim
+		git
+		wget
+		curl
+		htop
+		ranger
+		kubectl
+		kubernetes
+		fish
+		helix
+	];
+	programs.neovim =
+	{
+		enable = true;
+		defaultEditor = true;
+	};
+	programs.fish.enable = true;
+	system.stateVersion = "24.05";
 }
-
-
