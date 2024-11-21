a06bec5d71de7bfae1a2be69f6415f70cbe95fba
tu-leminh <51118002+tu-leminh@users.noreply.github.com>
2024-11-21 11:51:04 +0700

    first commit

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,89 +1,24 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 let
-kubeMasterHostname = "api.kube";
-kubeMasterAPIServerPort = 6443;
+        proxy="http://tulm9:EprSu3WQq5%402024@10.205.0.150:8080/";
 in
 {
-	imports =
-		[ 
-		./hardware-configuration.nix
-		];
-	boot.loader.systemd-boot.enable = true;
-	boot.loader.efi.canTouchEfiVariables = true;
-	networking.hostName = "dell"; 
-	networking.networkmanager.enable = true;
-	time.timeZone = "Asia/Ho_Chi_Minh";
-	i18n.defaultLocale = "en_US.UTF-8";
-	i18n.extraLocaleSettings = {
-		LC_ADDRESS = "vi_VN";
-		LC_IDENTIFICATION = "vi_VN";
-		LC_MEASUREMENT = "vi_VN";
-		LC_MONETARY = "vi_VN";
-		LC_NAME = "vi_VN";
-		LC_NUMERIC = "vi_VN";
-		LC_PAPER = "vi_VN";
-		LC_TELEPHONE = "vi_VN";
-		LC_TIME = "vi_VN";
-	};
-	services.xserver.enable = true;
-	services.xserver.displayManager.gdm.enable = true;
-	services.xserver.desktopManager.gnome.enable = true;
-	services.xserver.xkb = {
-		layout = "us";
-		variant = "";
-	};
-	services.printing.enable = true;
-	hardware.pulseaudio.enable = false;
-	security.rtkit.enable = true;
-	services.pipewire = 
-	{
-		enable = true;
-		alsa.enable = true;
-		alsa.support32Bit = true;
-		pulse.enable = true;
-	};
-	services.openssh.enable = true;
-	networking.firewall.enable = false;
-	users.users.mt = 
-	{
-		isNormalUser = true;
-		description = "mt";
-		extraGroups = [ "networkmanager" "wheel" ];
-		packages = with pkgs; 
-		[
-		];
-		shell = pkgs.fish;
-	};
-	nixpkgs.config.allowUnfree = true;
-	environment.systemPackages = with pkgs; 
-	[
-		neovim
-		git
-		wget
-		curl
-		htop
-		ranger
-		kubectl
-		kubernetes
-		fish
-	];
-	programs.fish.enable = true;
-	programs.neovim =
-	{
-		enable = true;
-		defaultEditor = true;
-	};
-	system.stateVersion = "24.05";
-	nix.settings.experimental-features = ["nix-command" "flakes"];
-	services.k3s = 
-	{
-		enable      =  true;
-		clusterInit =  true;
-		role = "server";
-		extraFlags  = "--disable=traefik --write-kubeconfig-mode=0644";
-	};
-	environment.variables = 
-	{
-		KUBECONFIG = "/etc/rancher/k3s/k3s.yaml";
-	};
+  imports = [
+    # include NixOS-WSL modules
+    <nixos-wsl/modules>
+  ];
+  networking.proxy.httpProxy = proxy;
+  networking.proxy.httpsProxy = proxy;
+  environment.variables =
+  {
+    HTTP_PROXY = proxy;
+    http_proxy = proxy;
+    https_proxy = proxy;
+    HTTPS_proxy = proxy;
+  };
+  wsl.enable = true;
+  wsl.defaultUser = "nixos";
+  system.stateVersion = "24.05";
 }
+
+
