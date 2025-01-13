6183ad09a6bf11c0835a00859cf7e283abce0d79
tu-leminh <51118002+tu-leminh@users.noreply.github.com>
2025-01-13 10:46:29 +0700

    Update default.nix

--- Home/default.nix
+++ Home/default.nix
@@ -8,7 +8,10 @@
 :
 {
 	imports = [
-	];
+	]
+        programs.kitty.enable = true; # required for the default Hyprland config
+        wayland.windowManager.hyprland.enable = true; # enable Hyprland
+        home.sessionVariables.NIXOS_OZONE_WL = "1";
 	# Minimal
 	home = {
 		username = "mt";
