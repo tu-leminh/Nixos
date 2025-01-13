e771e3206769c1ce151fc24197620f48d8d5203e
tu-leminh <51118002+tu-leminh@users.noreply.github.com>
2025-01-13 10:43:49 +0700

    Update default.nix

--- Home/default.nix
+++ Home/default.nix
@@ -4,23 +4,17 @@
 	config,
 	pkgs,
 	...
-}:{
+}
+:
+{
 	imports = [
 	];
-	nixpkgs = {
-		overlays = [
-		];
-		config = {
-			allowUnfree = true;
-			allowUnfreePredicate = _: true;
-		};
-	};
+	# Minimal
 	home = {
 		username = "mt";
 		homeDirectory = "/home/mt";
 	};
 	programs.home-manager.enable = true;
-	programs.git.enable = true;
 	systemd.user.startServices = "sd-switch";
 	home.stateVersion = "24.05";
 }
